e255d2c6b8e7bd6f116e1e2fe71850b4d0133687
Nik <lollee31@yandex.ru>
2026-03-26 00:15:42 +0300

    CORE-7 add theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import { Link, Route, Routes } from "react-router-dom";
-import "./index.scss";
+import "./styles/index.scss";
 
 import { AboutPageLazy } from "./Pages/AboutPage/AboutPageLazy";
 import { MainPageLazy } from "./Pages/MainPage/MainPageLazy";
 import { Suspense } from "react";
+import { useTheme } from "./theme/useTheme";
 
 const App = () => {
+  const { theme, toggleTheme } = useTheme();
+
   return (
-    <div className="app">
+    <div className={`app ${theme}`}>
+      <button onClick={toggleTheme}>Toggle</button>
       <Link to={"/"}> Главная</Link>
       <Link to={"/about"}> О нас</Link>
       <Suspense fallback={<div> Loading ... </div>}>
